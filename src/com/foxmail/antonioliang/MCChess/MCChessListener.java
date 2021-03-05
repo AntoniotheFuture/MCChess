0a7a3dea7f4b8c0650cc3e38c4b9a7e3cb5c6a2d
BuildTools <antonioliang@Foxmail.com>
2021-03-05 18:36:57 +0800

    更新readme和开源协议

--- src/com/foxmail/antonioliang/MCChess/MCChessListener.java
+++ src/com/foxmail/antonioliang/MCChess/MCChessListener.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2021 AntonioLiang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.foxmail.antonioliang.MCChess;
 
@@ -29,7 +39,7 @@ import org.bukkit.inventory.ItemStack;
 
 /**
  *
- * @author 54705
+ * @author AntonioLiang
  */
 //生物生成事件：设置生物AI
 //左键点击事件：放置时怪物删除/战斗中无效化
